8cb5a79c478f9575f7c4781da7036843b7ddfee1
lzyzxc <1019492420@qq.com>
2017-06-06 22:41:46 +0800

    Add files via upload

---
+++ Classes/Animation.cpp
@@ -0,0 +1,27 @@
+#include "Animation.h"
+
+
+CCAnimation * GameAction::CreationRoleRunAction(Vec2 direction,const char *name, int Num, float delay, unsigned int Loops) {
+	CCSpriteFrameCache * cache = CCSpriteFrameCache::sharedSpriteFrameCache();
+	Vector<SpriteFrame*> framesArray;
+	CCSpriteFrame * frame = NULL;
+	int k = 0;
+	if (direction == Vec2(0, 1))  k = 0;
+	if (direction == Vec2(0, -1)) k = 6;
+	if (direction == Vec2(-1, 0)) k = 12;
+	if (direction == Vec2(1, 0))  k = 18;
+	for (int i = 1; i<Num; i++) {
+		frame = cache->spriteFrameByName(CCString::createWithFormat("%s%d.png", name, k+i)->getCString());
+		if (frame == NULL) {
+			break;
+		}
+		framesArray.pushBack(frame);
+	}
+
+	CCAnimation * animation = CCAnimation::createWithSpriteFrames(framesArray);
+	animation->setLoops(Loops);
+	animation->setRestoreOriginalFrame(true); 
+	animation->setDelayPerUnit(delay);
+
+	return animation;
+}
---
+++ Classes/Animation.h
@@ -0,0 +1,14 @@
+#ifndef _ANIMATION_H_
+#define _ANIMATION_H_
+#include <cocos2d.h>
+#include <vector>
+using namespace cocos2d;
+using namespace std;
+class GameAction
+{
+public:
+	static CCAnimation * CreationRoleRunAction(Vec2 director,const char * name, int iNum, float delay, unsigned int iLoops);
+
+};
+
+#endif
